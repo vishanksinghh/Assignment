9aa7fd84e8f442ad362225e826029e426fd316cf
vishank.singhh <cu.16bcs2432@gmail.com>
2022-12-09 22:59:27 +0530

    my assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const intro = require('./introduction')
-const employee = require('./employee')
-const _ = require('underscore')
+const welcome=require('../logger/logger.js')
+const{printDate,printMonth,getBatchInfo}=require('../util/helper.js')
+const{trim,changeToUpperCase,changetoLowerCase}=require('../validator/formatter.js');
+
+const _ = require('lodash')
 
 router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
+    // problem 1
+    console.log(welcome())
+//  problem 2
+console.log(printDate());
+console.log(printMonth()+1);
+console.log(getBatchInfo());
 
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
+// problem 3
+
+let name="   Ankush " 
+console.log(trim(name))
+console.log(changeToUpperCase(name))
+console.log(changetoLowerCase(name))
+
+// problem 4
+
+let months=["january","february","march","april","may","june","july","august","september","october","november","december"]
+console.log(_.chunk(months,3));
+let odd=[1,3,5,7,9,11,13,15,17,19];
+console.log(_.tail(odd));
+
+let arr1=[3,4,5,8,3]
+let arr2=[3,4,5,2,77,8,3]
+let arr3=[3,4,1,10,11,5,8,3]
+let arr4=[3,0,5,8,3,11,1]
+let arr5=[3,4,5,8,3,65,77]
+console.log(_.union(arr1,arr2,arr3,arr4,arr5))
+
+console.log(_.fromPairs([["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]))
 
 
-    res.send('any dummy text')
+    
+
+
+    res.send('This is our domain')
 });
 
 
