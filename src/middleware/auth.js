6a10eb275a293f9e31174946c50a9e23f1ebcb51
vishank.singhh <cu.16bcs2432@gmail.com>
2022-12-27 18:17:45 +0530

    thisisAuth1

---
+++ src/middleware/auth.js
@@ -0,0 +1,32 @@
+const userModel=require('../models/userModel')
+const jwt=require('jsonwebtoken')
+
+const login=async(req,res,next)=>{
+
+    let useremail=req.body.emailId
+    let userpassword=req.body.password
+    // console.log(useremail)
+    // console.log(userpassword)
+    let user=await userModel.findOne({emailId:useremail,password:userpassword})
+    if(!user) return res.send({stauts:false,result:"check your email or password"})
+    
+    req.userDetails=user;
+    next()
+
+    
+}
+const verifytoken=(req,res,next)=>{
+    console.log(req.headers["x-auth-token"])
+    if(!req.headers) return res.send({result:"token not found"})
+
+let verifyToken=jwt.verify(req.headers["x-auth-token"],"VishankSingh",(err)=>{
+    if(err)
+    {res.send({result:"invalid token"})}
+})
+
+next()
+
+
+}
+
+module.exports={login,verifytoken}
